e4ecf46b7f3e6a0edac80054c20867c2c7f3c0ba
JamesSand <shazz20@mails.tsinghua.edu.cn>
2024-07-01 23:01:52 +0800

    done gen h dis

--- gen_data.py
+++ gen_data.py
@@ -3,7 +3,7 @@ import torch
 def gen_random_multi_gaussian(mu, sample_num, sigma_scale=1.0):
     # mu = torch.FloatTensor([1, 2, 0])
     multi_gaussian_dim = mu.shape[0]
-    sigma_scale = 10.0
+    # sigma_scale = 10.0
     # generate diagonal matrix
     sigma = torch.eye(multi_gaussian_dim)
 
@@ -13,19 +13,21 @@ def gen_random_multi_gaussian(mu, sample_num, sigma_scale=1.0):
     sampler = torch.distributions.MultivariateNormal(
         loc=mu, covariance_matrix=sigma
     )
-    samples: torch.Tensor = sampler.sample((100000, ))
+    samples: torch.Tensor = sampler.sample((sample_num, ))
     # samples.shape [100000, 3]
 
-    print(samples.shape)
+    return samples
 
-    # estimation of mu and 
-    new_mu = samples.mean(dim=0)
-    new_sigma = (samples - mu).T @ (samples - mu) / len(samples)
+    # print(samples.shape)
+
+    # # estimation of mu and 
+    # new_mu = samples.mean(dim=0)
+    # new_sigma = (samples - mu).T @ (samples - mu) / len(samples)
     
-    print(new_mu.round())
-    print(new_sigma.round())
+    # print(new_mu.round())
+    # print(new_sigma.round())
 
-    breakpoint()
-    print()
+    # breakpoint()
+    # print()
 
 
--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import torch
 
-mu = torch.FloatTensor([1, 2, 0])
-multi_gaussian_dim = mu.shape[0]
-sigma_scale = 10.0
-sigma = torch.eye(multi_gaussian_dim)
-sigma *= sigma_scale
-print(sigma)
-# sigma = torch.FloatTensor([
-#     [2, 0, 0],
-#     [0, 5, 0],
-#     [0, 0, 1]
-# ])
+from gen_data import gen_random_multi_gaussian
 
-sampler = torch.distributions.MultivariateNormal(
-    loc=mu, covariance_matrix=sigma
-)
-samples: torch.Tensor = sampler.sample((100000, ))
-# samples.shape [100000, 3]
+n = 100
 
-print(samples.shape)
+# generate m neuron projectors
+m = 10
+d = 5
 
-new_mu = samples.mean(dim=0)
-new_sigma = (samples - mu).T @ (samples - mu) / len(samples)
- 
-print(new_mu.round())
-print(new_sigma.round())
+def gen_h_dis(m, x_data):
+    # h^{dis}_{ij} = 1/m sum^m <<w_r, x_i> x_i, <w_r, x_j> x_j>
+    # h^{dis}_{ij} = <x_i, x_j> 1/m sum^m <w_r, x_i> <w_r, x_j>
+    # where <x_i, x_j>: n * n
+    # <w_r, x_i> <w_r, x_j>: m * n * n
+    # 1/m sum^m <w_r, x_i> <w_r, x_j>: n * n
 
-breakpoint()
-print()
+    # generate neurons: m * d shape
+    w_r = torch.randn(m, d)
+
+    # n * n
+    inner_xi_xj = x_data @ x_data.t()
+
+    # m * n
+    inner_wr_xi = w_r @ x_data.t()
+
+    # m * n * n
+    inner_wr_xi_inner_wr_xj = torch.empty(m, n, n)
+    
+    for iter_m in range(m):
+        inner_wr_xi_inner_wr_xj[iter_m] = inner_wr_xi[iter_m][..., None] @ inner_wr_xi[iter_m][None, ...]
+
+    # n * n
+    avg_inner_wr_xi_inner_wr_xj = inner_wr_xi_inner_wr_xj.mean(dim=0)
+
+    # n * n
+    h_dis = avg_inner_wr_xi_inner_wr_xj * inner_xi_xj
+
+    return h_dis
+
+    
 
 
